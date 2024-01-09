e9261ba617d7a2022bd7253def0fe4e54b249ee5
Germi <47084366+GerminalG@users.noreply.github.com>
2024-01-09 16:27:31 +0100

    ArrayNumberSequenceFix

--- A3ID1018/ArrayNumberSequence.java
+++ A3ID1018/ArrayNumberSequence.java
@@ -42,105 +42,125 @@ public class ArrayNumberSequence implements NumberSequence
 	}
 
     // add code here
-	//checks if the queue is empty by verifying if its elements array has a length of 0.
-	public boolean isEmpty ()
-	{
-		return numbers.length == 0;
-
-	}
-	//returns the number of elements
-	public int size ()
-	{
-		return numbers.length;
-	}
-	//adds a new element to the end of the queue
-	/***by creating a new array (a) with a length one greater than the current elements array, 
-	 * copying the elements from the current array, 
-	 * and adding the new element to the end */
-	public void put (double number)
-	{
-		double[] temp = new double[numbers.length + 1];
-		for (int i = 0; i < numbers.length; i++)
-		{
-			temp[i] = numbers[i];
-			temp[temp.length - 1] = number;
-			numbers = temp;
-
-		}
-
-
-	}
-	/**removes and returns the front element of the queue. 
-	 * It checks if the queue is empty and throws an IllegalStateException  */
-	public double take () throws IllegalStateException
-	{
-		if (this.isEmpty())
-		{
-			throw new IllegalStateException(
-								"take called on empty number sequence"
-			);
-
-		}
-		double n = numbers[0];
-		double[] temp = new double[numbers.length - 1];
-		for (int i = 0; i < numbers.length; i++)
-		{
-			temp[i] = numbers[i + 1];
-		}
-		numbers = temp;
-		return n;
-	}
-	//return the upper bound of the numbers in the sequence.
-	public double upperBound()
-	{
-		if (isEmpty())
-		{
-			throw new IllegalStateException("Sequence is empty");
-
-		}
-		double max = numbers[0];
-		for (int i = 0; i < numbers.length; i++)
-		{
-			if (numbers[i] > max) 
-			{
-				max = numbers[i];
-
-			}
-
-		}
-		return max;
-	}
-	//return the sequence as an array
-	public double[] asArray(){
-		//Arrays.copyOf() to create a copy of numbers array
-		return Arrays.copyOf(numbers, numbers.length);
-	}
-	//to find the position (index) of a given number in the sequence.
-	public int positionOf(double number) 
-	{
-        for (int i = 0; i < numbers.length; i++) 
-		{
-            if (numbers[i] == number)
-			 {
+		//returns the number of elements in the sequence. 
+	public int length() {
+        return numbers.length;  
+    }
+	// upperBound returns an upper bound for this sequence.
+	//The Stream interface provides two methods max() and min() 
+	//that return the largest and the smallest item from the underlying stream.
+    public double upperBound() {
+        return Arrays.stream(numbers).max().orElseThrow(); 
+    }
+	// lowerBound returns a lower bound for this sequence.
+	public double lowerBound() {
+        return Arrays.stream(numbers).min().orElseThrow(); 
+    }
+    // numberAt returns the number at the specified position
+    // in this sequence.
+    // The method throws IndexOutOfBoundsException if the
+    // position is wrong.
+	public double numberAt(int position) throws IndexOutOfBoundsException {
+        if (position < 0 || position >= numbers.length)
+            throw new IndexOutOfBoundsException("Invalid position");
+        return numbers[position];
+    }
+    // positionOf returns the position of the first occurance of
+    // the specified number in this sequence.
+    // If the number is not present in the sequence, -1 is returned.	
+	public int positionOf(double number) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == number) {
                 return i;
             }
         }
-        return -1;  // Return -1 if the number is not found in the sequence
+        return -1;
     }
-	//to check whether a given number is present in the sequence.
-	public boolean contains(double number) 
-	{
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == number) {
+    // isIncreasing returns true if this sequence is increasing,
+    // else false is returned.	
+	public boolean isIncreasing() {
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] <= numbers[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // isDecreasing returns true if this sequence is decreasing,
+    // else false is returned.
+	public boolean isDecreasing() {
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] >= numbers[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // contains returns true if this sequence contains the
+    // specified number, else false is returned.
+	public boolean contains(double number) {
+        for (double n : numbers) {
+            if (n == number) {
                 return true;
-			}
-		}
-		return false;
-	}
-	//returns the number of elements in the sequence. 
-	public int length() {
-        return numbers.length;  // Return the length of the numbers array
+            }
+        }
+        return false;
     }
+	// add adds the specified number to the end of this sequence.
+	public void add(double number) {
+        double[] newNumbers = Arrays.copyOf(numbers, numbers.length + 1);
+        newNumbers[numbers.length] = number;
+        numbers = newNumbers;
+    }
+	// insert inserts the given number at the specified position
+    // in this sequence.
+    // The method throws IndexOutOfBoundsException if the
+    // position is wrong.
+	public void insert(int position, double number) throws IndexOutOfBoundsException {
+        if (position < 0 || position > numbers.length)
+            throw new IndexOutOfBoundsException("Invalid position");
+
+        double[] newNumbers = new double[numbers.length + 1];
+		//0: The starting index in the numbers array from which to begin copying.
+		//   The starting index in the newNumbers array where elements are copied.
+		//ensures that the elements before the insertion point are preserved in the new array.
+		//Basically, first part, from first element to new number(not included)
+        System.arraycopy(numbers, 0, newNumbers, 0, position);
+        newNumbers[position] = number;
+		//second part, newnumber to end.
+		//The "position + 1" is used to account for the space left for the inserted element.
+        System.arraycopy(numbers, position, newNumbers, position + 1, numbers.length - position);
+
+        numbers = newNumbers;
+    }
+	// removeAt removes the number at the specified position
+    // in this sequence.
+    // The method throws IndexOutOfBoundsException if the
+    // position is wrong.
+    // The method throws IllegalStateException if there are
+    // just two numbers in the sequence.
+	public void removeAt(int position) throws IndexOutOfBoundsException, IllegalStateException {
+        if (position < 0 || position >= numbers.length)
+            throw new IndexOutOfBoundsException("Invalid position");
+        if (numbers.length == 2)
+            throw new IllegalStateException("Cannot remove element when there are only two elements");
+
+        double[] newNumbers = new double[numbers.length - 1];
+        System.arraycopy(numbers, 0, newNumbers, 0, position);
+        System.arraycopy(numbers, position + 1, newNumbers, position, numbers.length - position - 1);
+
+        numbers = newNumbers;
+    }
+	// asArray returns an array containing all of the numbers in
+    // this sequence, in the same order as in the sequence.
+	public double[] asArray() {
+        return Arrays.copyOf(numbers, numbers.length);
+    }
+
+
+
+
+
 }
 
 
